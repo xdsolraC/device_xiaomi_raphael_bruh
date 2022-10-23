d6aa08561d40946da977b6855557b8b9f0170af1
xdsolraC <xds0lr4c@gmail.com>
2022-10-23 07:30:17 +0000

    switch to aosp clang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,4 +8,3 @@ git clone https://github.com/VoltageOS/hardware_qcom_media.git -b 12-sm8150 hard
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-20.0 packages/resources/devicesettings
 git clone https://github.com/VoltageOS-Devices/vendor_xiaomi_raphael.git -b 13 vendor/xiaomi/raphael --depth=1
 git clone https://github.com/xdsolraC/kernel_xiaomi_raphael.git -b 13 kernel/xiaomi/raphael --depth=1
-git clone https://gitlab.com/varunhardgamer/trb_clang.git -b 16 prebuilts/clang/host/linux-x86/trb_clang --depth=1
