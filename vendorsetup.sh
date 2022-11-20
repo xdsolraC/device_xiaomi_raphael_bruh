4395b1d92f65af4e74d746961602ed8034c42198
xdsolraC <xds0lr4c@gmail.com>
2022-11-20 08:24:20 +0000

    update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,4 @@
 rm -rf hardware/qcom-caf/sm8150/display
-rm -rf packages/resources/devicesettings
 git clone https://github.com/xdsolraC/hardware_qcom_display.git -b 12-sm8150 hardware/qcom-caf/sm8150/display
-git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-20.0 packages/resources/devicesettings
 git clone https://github.com/VoltageOS-Devices/vendor_xiaomi_raphael.git -b 13 vendor/xiaomi/raphael --depth=1
-git clone https://github.com/xdsolraC/kernel_xiaomi_raphael-test.git -b 13.0 kernel/xiaomi/raphael --depth=1
+git clone https://github.com/Evolution-X-Devices/kernel_xiaomi_raphael.git -b tiramisu kernel/xiaomi/raphael --depth=1
